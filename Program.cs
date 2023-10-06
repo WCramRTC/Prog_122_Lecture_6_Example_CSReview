02fb1552c88c22a17547cb5f0ef05a882f329c8f
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-05 18:28:39 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Prog_122_Lecture_6_Example_CSReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Variables
+            // C# is strongly typed language
+            // we DECLARE the TYPE when we FIRST INITIALIZE
+            // string, int, double, bool
+            // String - a collection of characters
+            //  - character : single symbol surround by single quotes
+            // 'a'
+            // int - whole numbers
+            // double - decimal numbers
+            // bool - true or false
+
+            string name = "Will";
+            char singleLetter = 'C';
+
+            // int and double
+            int wholeNumber;
+            //sbyte range = 128;
+            byte unsigned = 255;
+
+            // HOw to comment out a line hotkey
+            // Ctrl + K, Ctrl + C
+            // Uncomment is 
+            // Ctrl + K, Ctrl + U
+
+            // Copy is Ctrl + C
+            // Paste is Ctrl + V
+            // Cut is Ctrl + X
+            // Undo is Ctrl + Z
+
+            string iWantToCutThisLine = "This Line";
+
+
+
+
+            // Loops
+            // Conditions
+            // Operations
+            // Arrays
+            // Scope
+            // Methods
+
+
+        } // Main
+
+    } // class
+
+} // namespace
